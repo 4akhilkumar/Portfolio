5048b5b0d63de967208266bc881bee752c0eb8bb
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2023-04-30 12:56:40 +0530

    Removed unused static files, Fetching logo and profile image from json file
    Some SEO work

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
+import { client } from '../../client';
+
 import './Navbar.scss';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [headersData, setHeadersData] = useState([]);
+
+  useEffect(() => {
+    client().then((data) => {
+      const headerdata = data.Header;
+      setHeadersData(headerdata);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }, []);
 
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <img src={headersData.logo} alt={headersData.nickname} />
       </div>
       <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
